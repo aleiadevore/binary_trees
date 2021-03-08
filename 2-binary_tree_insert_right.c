b5f4d0e01e1a002aff2563e9b6642d6df705cc38
blanketmanatee <ashleyl.welch@gmail.com>
2021-03-08 14:47:15 -0600

    task 2 and initial push to branch

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_insert_left - inserts node as left child
+* @parent: parent node
+* @value: node's value
+* Return: pointer to new node
+*/
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new = NULL, *oldright = NULL;
+
+	if (!parent)
+		return (NULL);
+	new = binary_tree_node(parent, value);
+
+	/* checking if parent has left node already */
+	if (!new)
+		return (NULL);
+	if (parent->right != NULL)
+		oldright = parent->right;
+		if (oldright)
+		{
+			new->right = oldright;
+			oldright->parent = new;
+		}
+	parent->right = new;
+	return (new);
+}

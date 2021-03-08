af9cfb066a57a2f16b4705623e27703b4dae6149
blanketmanatee <ashleyl.welch@gmail.com>
2021-03-08 17:16:16 -0600

    updating my branch

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -15,7 +15,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		return (NULL);
 	new = binary_tree_node(parent, value);
 
-	/* checking if parent has left node already */
+	/* checking if parent has right node already */
 	if (!new)
 		return (NULL);
 	if (parent->right != NULL)

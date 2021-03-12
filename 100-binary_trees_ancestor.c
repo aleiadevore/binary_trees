afe308e139a151c6bfc117730f64136aea434c3e
blanketmanatee <ashleyl.welch@gmail.com>
2021-03-11 21:20:48 -0600

    return values

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -7,19 +7,20 @@
  * Return: pointer to lowest common ancestor node. If no common return NULL
  */
 
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, 
+    const binary_tree_t *second)
 {
-    binary_tree_t *left, *right;
+	binary_tree_t *left, *right;
 
-    if (first == NULL || second == NULL)
-        return (NULL);
-    if (first == second)
-        return((binary_tree_t*)first);
+	if (first == NULL || second == NULL)
+		return (NULL);
+	if (first == second)
+		return((binary_tree_t *) first);
 
-    left = first->parent, right = second->parent;
-    if (first == second)
-        return ((binary_tree_t*)first);
-    second = second->parent;
-    first = first->parent;
-    
+	left = first->parent, right = second->parent;
+	if (first == second)
+		return ((binary_tree_t *) first);
+	second = second->parent;
+	first = first->parent;
+return (NULL);
 }
